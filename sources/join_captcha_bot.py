398661b29ea864466764ffebb099ddf0e7138a49
erkutorenk <52276842+erkutorenk@users.noreply.github.com>
2019-11-15 00:58:33 +0400

    Update join_captcha_bot.py

--- sources/join_captcha_bot.py
+++ sources/join_captcha_bot.py
@@ -760,7 +760,7 @@ def msg_nocmd(bot, update):
             bot_msg = TEXT[lang]["CAPTCHA_SOLVED"].format(new_user["user_name"])
             # Uncomment and use next first line instead the next ones, if we want Bot to 
             # auto-remove captcha solved message too after 60mins
-            tlg_send_selfdestruct_msg(bot, chat_id, bot_msg, 1)
+            tlg_send_selfdestruct_msg_in(bot, chat_id, bot_msg, 1)
             #try:
                 #bot.send_message(chat_id, bot_msg)
             #except Exception as e:
